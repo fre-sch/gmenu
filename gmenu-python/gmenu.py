e75eea47e696e12f9eedda77f1167692b25acf5c
Frederik Schumacher <frederik.schumacher@googlemail.com>
2010-05-24 10:30:13 +0200

    gmenu.py now does everything gmenu.c does

--- gmenu-python/gmenu.py
+++ gmenu-python/gmenu.py
@@ -1,78 +1,147 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# vim:et ts=4 sw=4:
+import gobject
 import gtk
 import sys
 import os
 
-width = 240
-height = 320
-items = set()
+class GMenu( gtk.Window ):
+    def __init__( self ):
+        super( GMenu, self ).__init__( gtk.WINDOW_TOPLEVEL )
+        self.set_position( gtk.WIN_POS_CENTER )
+        self.set_default_size( 240, 320 )
+        self.set_type_hint( gtk.gdk.WINDOW_TYPE_HINT_DIALOG );
+        self.set_border_width( 2 )
+        
+        self.queue_refilter_id = 0
+        self.on_entry_key_press_id = 0
 
-def read_stdin():
-	for line in sys.stdin: items.add( line.strip() )
+        self.init_entry()
+        self.init_model()
+        sw = self.init_view()
 
-class View( gtk.TreeView ):
-	def __init__( self, *args, **kwargs ):
-		super( View, self ).__init__( *args, **kwargs )
-		self.set_headers_visible( False )
-		self.set_search_column( 0 )
-		renderer = gtk.CellRendererText()
-		col = gtk.TreeViewColumn( None, renderer, text=0 )
-		self.append_column( col )
-		self.unset_flags( gtk.CAN_DEFAULT | gtk.CAN_FOCUS )
+        lbox = gtk.VBox( False, 2 )
+        lbox.pack_start( self.entry, False, True )
+        lbox.pack_start( sw, True, True )
+        self.add( lbox )
 
-class Entry( gtk.Entry ):
-	def __init__( self, *args, **kwargs ):
-		super( Entry, self ).__init__( *args, **kwargs )
-		self.set_flags( gtk.CAN_DEFAULT | gtk.CAN_FOCUS )
+    def init_entry( self ):
+        self.entry = gtk.Entry()
+        self.on_entry_key_press_id = self.entry.connect(
+            "key-press-event", self.on_entry_key_press )
+        self.entry.connect( "changed", self.on_entry_changed )
 
-class Window( gtk.Window ):
-	def __init__( self ):
-		super( Window, self ).__init__( gtk.WINDOW_POPUP )
-		self.set_position( gtk.WIN_POS_CENTER_ALWAYS )
-		self.set_default_size( width, height )
-		
-		self.entry = Entry()
-		self.entry.connect( "key-press-event", self.on_entry_key_press )
+    def init_model( self ):
+        model = gtk.ListStore( str )
+        for line in sys.stdin:
+            model.append( (line.strip(),) )
+        
+        self.filter = model.filter_new()
+        self.filter.set_visible_func( self.visible_func )
 
-		model = gtk.ListStore( str )
-		for item in items: model.append( (item,) )
-		
-		self.filter = model.filter_new()
-		self.filter.set_visible_func( self.visible_func )
-		
-		view = View( self.filter )
-		sw = gtk.ScrolledWindow()
-		sw.set_policy( gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC )
-		sw.set_shadow_type( gtk.SHADOW_IN )
-		sw.add( view )
-		
-		lbox = gtk.VBox( False, 0)
-		lbox.set_border_width( 2 )
-		lbox.pack_start( self.entry, False, True )
-		lbox.pack_start( sw, True, True )
-	
-		box = gtk.Frame()
-		box.set_border_width( 0 )
-		box.set_shadow_type( gtk.SHADOW_OUT )
-		box.add( lbox )
-		
-		self.add( box )
-	
-	def show_all( self ):
-		super( Window, self ).show_all()
-		gtk.gdk.keyboard_grab( self.window )
-	
-	def on_entry_key_press( self, entry, event ):
-		if event.keyval == gtk.keysyms.Escape:
-			gtk.main_quit()
-		self.filter.refilter()
-	
-	def visible_func( self, model, itr ):
-		search = self.entry.get_text()
-		if len( search ):
-			return -1 != model.get_value( itr, 0 ).find( search )
-		return True
+    def init_view( self ):
+        self.view = gtk.TreeView( self.filter )
+        self.view.set_headers_visible( False )
+        self.view.set_search_column( 0 )
+
+        renderer = gtk.CellRendererText()
+        renderer.ellipsize = True
+        self.view.insert_column_with_data_func(
+            -1, "Data", renderer, self.cellrenderer_data_func )
+
+        sw = gtk.ScrolledWindow()
+        sw.set_policy( gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC )
+        sw.set_shadow_type( gtk.SHADOW_IN )
+        sw.add( self.view )
+        return sw
+        
+    def on_entry_changed( self, entry ):
+        if self.queue_refilter_id:
+            gobject.source_remove( self.queue_refilter_id )
+        self.queue_refilter_id = gobject.timeout_add( 300, self.refilter )
+
+    def refilter( self ):
+        self.filter.refilter()
+        self.queue_refilter_id = 0
+
+    def select_next( self ):
+        sel = self.view.get_selection()
+        mdl, itr = sel.get_selected()
+        itrok = False
+        if itr:
+            itr = mdl.iter_next( itr )
+            itrok = itr is not None
+        if not itrok:
+            itr = mdl.get_iter_first()
+            itrok = itr is not None
+        if itrok:
+            sel.select_iter( itr )
+
+    def select_last( self ):
+        sel = self.view.get_selection()
+        mdl, itr = sel.get_selected()
+        itr = mdl.get_iter_first()
+        last = None
+        while itr:
+            last = itr
+            itr = mdl.iter_next( itr )
+        last and sel.select_iter( last )
+
+    def select_previous( self ):
+        sel = self.view.get_selection()
+        mdl, itr = sel.get_selected()
+        if itr:
+            path = mdl.get_path( itr )
+            path = ( path[0] - 1, )
+            if path[0] >= 0:
+                sel.select_path( path )
+            else:
+                self.select_last()
+        else:
+            self.select_last()
+
+    def on_entry_key_press( self, entry, e ):
+        if e.keyval == gtk.keysyms.Escape:
+            gtk.main_quit()
+            return True
+        elif e.keyval == gtk.keysyms.Tab or e.keyval == gtk.keysyms.Down:
+            self.select_next()
+            return True
+        elif e.keyval == gtk.keysyms.Up:
+            self.select_previous()
+            return True
+        elif e.keyval == gtk.keysyms.Return:
+            self.print_result()
+            gtk.main_quit()
+            return True
+        return False
+
+    def print_result( self ):
+        sel = self.view.get_selection()
+        mdl, itr = sel.get_selected()
+        if itr:
+            print mdl.get_value( itr, 0 )
+        else:
+            print self.entry.get_text()
+
+    def visible_func( self, model, itr ):
+        search = self.entry.get_text()
+        if len( search ):
+            return -1 != model.get_value( itr, 0 ).find( search )
+        return True
+
+    def cellrenderer_data_func( self, col, cell, mdl, itr ):
+        search = self.entry.get_text()
+        item = mdl.get_value( itr, 0 )
+        lead_p = item.find( search )
+        if search and lead_p != -1:
+            lead = item[:lead_p]
+            tail = item[lead_p + len(search):]
+            cell.set_property( "markup", lead + "<b>" + search + "</b>" + tail )
+        else:
+            cell.set_property( "markup", item )
 
 if __name__ == "__main__":
-	read_stdin()
-	Window().show_all()
-	gtk.main()
+    GMenu().show_all()
+    gtk.main()
